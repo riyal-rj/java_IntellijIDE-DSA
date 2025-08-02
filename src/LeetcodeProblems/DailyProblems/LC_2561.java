02f857d18985c7f08dcfa39ef8c28e83b0fb3e1e
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-08-02 20:52:34 +0530

    2nd aug 2025 daily challenge

---
+++ src/LeetcodeProblems/DailyProblems/LC_2561.java
@@ -0,0 +1,10 @@
+package LeetcodeProblems.DailyProblems;
+
+public class LC_2561 {
+    public static void main(String[] args) {
+        int []basket1={4,2,2,2};
+        int []basket2={1,4,1,2};
+    }
+
+
+}
