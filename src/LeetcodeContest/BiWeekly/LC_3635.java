4fd26afba0d735088f52330066bdc4df9fbbf387
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-08-03 20:31:25 +0530

    Biweekly Contest 162 problem 3

--- src/LeetcodeContest/BiWeekly/LC_3635.java
+++ src/LeetcodeContest/BiWeekly/LC_3635.java
@@ -6,16 +6,18 @@ public class LC_3635 {
     }
     public static int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
        int minTime = Integer.MAX_VALUE;
+       int mini= Integer.MAX_VALUE;
        for(int i=0;i<landStartTime.length;i++)
            minTime=Math.min(minTime, landStartTime[i] + landDuration[i]);
        for(int i=0;i<waterStartTime.length;i++)
-           minTime=Math.min(minTime, waterDuration[i]+Math.max(minTime,waterStartTime[i]));
+           mini=Math.min(mini, waterDuration[i]+Math.max(minTime,waterStartTime[i]));
 
+       minTime=Integer.MAX_VALUE;
        for(int i=0;i<waterStartTime.length;i++)
            minTime=Math.min(minTime, waterStartTime[i]+waterDuration[i]);
 
        for(int i=0;i<landStartTime.length;i++)
-           minTime=Math.min(minTime,landDuration[i]+Math.max(minTime,landStartTime[i]));
-       return  minTime;
+           mini=Math.min(mini,landDuration[i]+Math.max(minTime,landStartTime[i]));
+       return  mini;
     }
 }
